fce437cafd98f618712c71f824bb6e03f4067a4a
Pratanu Santra <pratanusantra0098@gmail.com>
2023-12-24 01:59:36 +0530

    cloning

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { createRoot } from "react-dom";
+import ReactDOM from "react-dom/client";
+
 import App from "./App";
 
-import "./global.css";
-
-createRoot(document.querySelector("#root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
